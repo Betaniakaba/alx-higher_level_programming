9478052e5417feff79441064f255f517eddcdec4
Betaniakaba <Betania.berhanu@outlook.com>
2022-12-01 12:12:03 -0800

    Fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
-""Print the alphabet in lowercase, not followed by a new line."""
+"""Print the alphabet in lowercase, not followed by a new line.
+"""
 
 for letter in range(97, 123):
 
