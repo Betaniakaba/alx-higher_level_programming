7cb53eba2965d5c1313656547818cb1788ae0032
Betaniakaba <Betania.berhanu@outlook.com>
2022-12-01 05:03:54 -0800

    if else loop

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 10-add.py
+# Betania B kaba <betania.berhanu@outlook.com>
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 12-fizzbuzz.py
+# Betania B kaba <betania.berhanu@outlook.com>
+
+def fizzbuzz():
+
+	   """ For Multiples of 3 and 5, print FizzBuzz.
+	   """
+	   for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+		""""For Multiples of 3 print Fizz.
+		"""
+		elif number % 3 == 0:
+            print("Fizz ", end="")
+		"""For Multiples of 5 print Buzz.
+		"""
+		elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
