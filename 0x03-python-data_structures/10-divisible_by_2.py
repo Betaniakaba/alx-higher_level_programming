21d6a1b18a2a79b4b6e1b54b679e15cc3bdb7614
Betaniakaba <Betania.berhanu@outlook.com>
2022-12-07 23:26:29 -0800

    0x03

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]);
+
+    """First find the multiples of two in the list"""
+
+    multiples = []
+
+    for i in range(len(my_list)):
+
+        if my_list[i] % 2 == 0:
+
+            multiples.append(True)
+
+        else:
+
+            multiples.append(False)
+
+    return (multiples)
